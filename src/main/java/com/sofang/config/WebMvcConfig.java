9681d80bed61b6ae0777cea42b3aca934aa10170
geguofeng <849447894@qq.com>
2018-03-07 00:09:34 +0800

    404,statusCode,拦截器开发

--- src/main/java/com/sofang/config/WebMvcConfig.java
+++ src/main/java/com/sofang/config/WebMvcConfig.java
@@ -1,4 +1,75 @@
 package com.sofang.config;
 
-public class WebMvcConfig {
+import org.hibernate.resource.jdbc.ResourceRegistry;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+/**
+ * Created by gegf
+ */
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter
+        implements ApplicationContextAware{
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * 静态资源加载配置
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    /**
+     * 模板资源解析器
+     * @return
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.thymeleaf")
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+
+    /**
+     * Thymeleaf 方言解析器
+     * @return
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);//支持EL
+        return templateEngine;
+    }
+
+    /**
+     * 视图解析器
+     * @return
+     */
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(this.templateEngine());
+        return viewResolver;
+    }
 }
--- src/main/java/com/sofang/controller/HomeController.java
+++ src/main/java/com/sofang/controller/HomeController.java
@@ -1,4 +1,27 @@
 package com.sofang.controller;
 
+import com.sofang.base.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by gegf
+ */
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("name", "SoFang");
+        return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/get")
+    public ResponseEntity get(){
+        return ResponseEntity.createBySuccessMessage("Success!");
+    }
 }
