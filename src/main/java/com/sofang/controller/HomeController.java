23dc16b2c68051f6d324c46f6a7d6538081aecb2
geguofeng <849447894@qq.com>
2018-03-08 23:57:51 +0800

    update

--- src/main/java/com/sofang/controller/HomeController.java
+++ src/main/java/com/sofang/controller/HomeController.java
@@ -13,10 +13,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("name", "SoFang");
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout/page")
+    public String logoutPage() {
+        return "logout";
     }
 
     @ResponseBody
