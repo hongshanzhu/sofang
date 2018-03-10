af4ca93d03fade95b22c8108897ec089463f3c27
geguofeng <849447894@qq.com>
2018-03-10 23:58:49 +0800

    登录功能完善

--- src/main/java/com/sofang/controller/admin/AdminController.java
+++ src/main/java/com/sofang/controller/admin/AdminController.java
@@ -1,7 +1,15 @@
 package com.sofang.controller.admin;
 
+import com.sofang.base.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.*;
 
 /**
  * 后台管理
@@ -33,5 +41,11 @@ public class AdminController {
     @GetMapping("/admin/add/house")
     public String addHousePage(){
         return "admin/house-add";
+    }
+
+    @ResponseBody
+    @PostMapping(value="/admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity uploadPhoto(@RequestParam("file")MultipartFile file){
+        return ResponseEntity.createBySuccessMessage("");
     }
 }
