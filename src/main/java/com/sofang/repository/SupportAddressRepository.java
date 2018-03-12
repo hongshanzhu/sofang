a4c99e055b86ca4a66c3c0a60a63d8ef5279c2a0
geguofeng <849447894@qq.com>
2018-03-12 23:52:38 +0800

    房源信息开发

---
+++ src/main/java/com/sofang/repository/SupportAddressRepository.java
@@ -0,0 +1,13 @@
+package com.sofang.repository;
+
+import com.sofang.entity.SupportAddress;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by gegf
+ */
+public interface SupportAddressRepository extends CrudRepository<SupportAddress, Long>{
+
+
+
+}
---
+++ src/main/java/com/sofang/web/controller/house/HouseController.java
@@ -0,0 +1,12 @@
+package com.sofang.web.controller.house;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by gegf
+ */
+@Controller
+public class HouseController {
+
+
+}
