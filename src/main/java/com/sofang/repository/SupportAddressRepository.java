b82fe0ac2c11b9345beacd955073ae3c2402099c
geguofeng <849447894@qq.com>
2018-03-13 22:35:55 +0800

    房源信息开发

--- src/main/java/com/sofang/repository/SupportAddressRepository.java
+++ src/main/java/com/sofang/repository/SupportAddressRepository.java
@@ -3,11 +3,19 @@ package com.sofang.repository;
 import com.sofang.entity.SupportAddress;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by gegf
  */
 public interface SupportAddressRepository extends CrudRepository<SupportAddress, Long>{
 
+    List<SupportAddress> findAllByLevel(String level);
 
+    SupportAddress findByEnNameAndLevel(String enName, String level);
+
+    SupportAddress findByEnNameAndBelongTo(String enName, String belongTo);
+
+    List<SupportAddress> findAllByLevelAndBelongTo(String level, String belongTo);
 
 }
---
+++ src/main/java/com/sofang/service/AddressService.java
@@ -0,0 +1,29 @@
+package com.sofang.service;
+
+import java.util.Map;
+
+import com.sofang.base.Level;
+
+import com.sofang.base.ServiceMultiResult;
+import com.sofang.web.dto.SupportAddressDTO;
+
+/**
+ * 地址服务接口
+ * Created by gegf.
+ */
+public interface AddressService {
+    /**
+     * 获取所有支持的城市列表
+     * @return
+     */
+    ServiceMultiResult<SupportAddressDTO> findAllCities();
+
+    /**
+     * 根据英文简写获取具体区域的信息
+     * @param cityEnName
+     * @param regionEnName
+     * @return
+     */
+    Map<Level, SupportAddressDTO> findCityAndRegion(String cityEnName, String regionEnName);
+}
+
