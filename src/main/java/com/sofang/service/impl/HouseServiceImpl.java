9a67926511d47f709d0ce061e9e732cf24a8e377
geguofeng <849447894@qq.com>
2018-03-16 23:14:20 +0800

    fang house

--- src/main/java/com/sofang/service/impl/HouseServiceImpl.java
+++ src/main/java/com/sofang/service/impl/HouseServiceImpl.java
@@ -1,18 +1,24 @@
 package com.sofang.service.impl;
 
+import com.google.common.collect.Lists;
 import com.sofang.base.LoginUserUtil;
-import com.sofang.base.ServiceMultiResult;
 import com.sofang.base.ServiceResult;
-import com.sofang.entity.House;
-import com.sofang.repository.HouseRepository;
+import com.sofang.entity.*;
+import com.sofang.repository.*;
 import com.sofang.service.HouseService;
 import com.sofang.web.dto.HouseDTO;
+import com.sofang.web.dto.HouseDetailDTO;
+import com.sofang.web.dto.HousePictureDTO;
 import com.sofang.web.form.HouseForm;
+import com.sofang.web.form.PhotoForm;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class HouseServiceImpl implements HouseService {
@@ -23,8 +29,33 @@ public class HouseServiceImpl implements HouseService {
     @Autowired
     private HouseRepository houseRepository;
 
+    @Autowired
+    private SubwayRepository subwayRepository;
+
+    @Autowired
+    private SubwayStationRepository subwayStationRepository;
+
+    @Autowired
+    private HouseDetailRepository houseDetailRepository;
+
+    @Autowired
+    private HousePictureRepository housePictureRepository;
+
+    @Autowired
+    private HouseTagRepository houseTagRepository;
+
+    @Value("${qiniu.cdn.prefix}")
+    private String cdnPrefix;
+
+
     @Override
     public ServiceResult<HouseDTO> save(HouseForm houseForm) {
+        HouseDetail detail = new HouseDetail();
+        ServiceResult<HouseDTO> subwayValidtionResult = wrapperDetailInfo(detail, houseForm);
+        if (subwayValidtionResult != null) {
+            return subwayValidtionResult;
+        }
+
         House house = new House();
         modelMapper.map(houseForm, house);
         Date now = new Date();
@@ -33,6 +64,82 @@ public class HouseServiceImpl implements HouseService {
         house.setAdminId(LoginUserUtil.getLoginUser());
         houseRepository.save(house);
         //TODO HouseDetail
+        detail.setHouseId(house.getId());
+        detail = houseDetailRepository.save(detail);
+
+        List<HousePicture> pictures = generatePictures(houseForm, house.getId());
+        Iterable<HousePicture> housePictures = housePictureRepository.save(pictures);
+
+        HouseDTO houseDTO = modelMapper.map(house, HouseDTO.class);
+        HouseDetailDTO houseDetailDTO = modelMapper.map(detail, HouseDetailDTO.class);
+
+        houseDTO.setHouseDetail(houseDetailDTO);
+
+        List<HousePictureDTO> pictureDTOS = Lists.newArrayList();
+        housePictures.forEach(housePicture -> pictureDTOS.add(modelMapper.map(housePicture, HousePictureDTO.class)));
+        houseDTO.setPictures(pictureDTOS);
+        houseDTO.setCover(this.cdnPrefix + houseDTO.getCover());
+
+        List<String> tags = houseForm.getTags();
+        if (tags != null && !tags.isEmpty()) {
+            List<HouseTag> houseTags = new ArrayList<>();
+            for (String tag : tags) {
+                houseTags.add(new HouseTag(house.getId(), tag));
+            }
+            houseTagRepository.save(houseTags);
+            houseDTO.setTags(tags);
+        }
+
+        return new ServiceResult<HouseDTO>(true, null, houseDTO);
+    }
+
+    /**
+     * 图片对象列表信息填充
+     * @param form
+     * @param houseId
+     * @return
+     */
+    private List<HousePicture> generatePictures(HouseForm form, Long houseId) {
+        List<HousePicture> pictures = Lists.newArrayList();
+        if (form.getPhotos() == null || form.getPhotos().isEmpty()) {
+            return pictures;
+        }
+
+        for (PhotoForm photoForm : form.getPhotos()) {
+            HousePicture picture = new HousePicture();
+            picture.setHouseId(houseId);
+            picture.setCdnPrefix(cdnPrefix);
+            picture.setPath(photoForm.getPath());
+            picture.setWidth(photoForm.getWidth());
+            picture.setHeight(photoForm.getHeight());
+            pictures.add(picture);
+        }
+        return pictures;
+    }
+
+    private ServiceResult<HouseDTO> wrapperDetailInfo(HouseDetail houseDetail, HouseForm houseForm) {
+        Subway subway = subwayRepository.findOne(houseForm.getSubwayLineId());
+        if (subway == null) {
+            return new ServiceResult<>(false, "Not valid subway line!");
+        }
+
+        SubwayStation subwayStation = subwayStationRepository.findOne(houseForm.getSubwayStationId());
+        if (subwayStation == null || subway.getId() != subwayStation.getSubwayId()) {
+            return new ServiceResult<>(false, "Not valid subway station!");
+        }
+
+        houseDetail.setSubwayLineId(subway.getId());
+        houseDetail.setSubwayLineName(subway.getName());
+
+        houseDetail.setSubwayStationId(subwayStation.getId());
+        houseDetail.setSubwayStationName(subwayStation.getName());
+
+        houseDetail.setDescription(houseForm.getDescription());
+        houseDetail.setDetailAddress(houseForm.getDetailAddress());
+        houseDetail.setLayoutDesc(houseForm.getLayoutDesc());
+        houseDetail.setRentWay(houseForm.getRentWay());
+        houseDetail.setRoundService(houseForm.getRoundService());
+        houseDetail.setTraffic(houseForm.getTraffic());
         return null;
     }
 }
