d1a08654166d294280a5ec0edc409cacd2f2312b
rageudream <849447894@qq.com>
2018-05-17 23:54:20 +0800

    完善房源信息

--- src/main/java/com/sofang/web/controller/admin/AdminController.java
+++ src/main/java/com/sofang/web/controller/admin/AdminController.java
@@ -1,9 +1,11 @@
 package com.sofang.web.controller.admin;
 
+import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.qiniu.common.QiniuException;
 import com.qiniu.http.Response;
 import com.sofang.base.*;
+import com.sofang.entity.SupportAddress;
 import com.sofang.service.AddressService;
 import com.sofang.service.HouseService;
 import com.sofang.service.QiNiuService;
@@ -87,9 +89,6 @@ public class AdminController {
         dataTablesResponse.setDraw(search.getDraw());
         return dataTablesResponse;
     }
-
-
-
 
     /**
      * 上传图片接口
@@ -177,6 +176,151 @@ public class AdminController {
             model.addAttribute("station", subwayStationDTOServiceResult.getResult());
         }
         return "admin/house-edit";
+    }
+
+    /**
+     * 编辑接口
+     */
+    @PostMapping("admin/house/edit")
+    @ResponseBody
+    public ResponseEntity saveHouse(@Valid @ModelAttribute("form-house-edit") HouseForm houseForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST.value(), bindingResult.getAllErrors().get(0).getDefaultMessage(), null);
+        }
+
+        Map<Level, SupportAddressDTO> addressMap = addressService.findCityAndRegion(houseForm.getCityEnName(), houseForm.getRegionEnName());
+
+        if (addressMap.keySet().size() != 2) {
+            return ResponseEntity.ofSuccess(StatusCode.NOT_VALID_PARAM);
+        }
+
+        ServiceResult result = houseService.update(houseForm);
+        if (result.isSuccess()) {
+            return ResponseEntity.ofSuccess(null);
+        }
+
+        ResponseEntity response = ResponseEntity.createByErrorCodeMessage(StatusCode.BAD_REQUEST);
+        response.setMessage(result.getMessage());
+        return response;
+    }
+
+    /**
+     * 移除图片接口
+     * @param id
+     * @return
+     */
+    @DeleteMapping("admin/house/photo")
+    @ResponseBody
+    public ResponseEntity removeHousePhoto(@RequestParam(value = "id") Long id) {
+        ServiceResult result = this.houseService.removePhoto(id);
+
+        if (result.isSuccess()) {
+            return ResponseEntity.createBySuccess();
+        } else {
+            return ResponseEntity.createByErrorCodeMessage(HttpStatus.BAD_REQUEST.value(), result.getMessage());
+        }
+    }
+
+    /**
+     * 修改封面接口
+     * @param coverId
+     * @param targetId
+     * @return
+     */
+    @PostMapping("admin/house/cover")
+    @ResponseBody
+    public ResponseEntity updateCover(@RequestParam(value = "cover_id") Long coverId,
+                                   @RequestParam(value = "target_id") Long targetId) {
+        ServiceResult result = this.houseService.updateCover(coverId, targetId);
+
+        if (result.isSuccess()) {
+            return ResponseEntity.createBySuccess();
+        } else {
+            return ResponseEntity.createByErrorCodeMessage(HttpStatus.BAD_REQUEST.value(), result.getMessage());
+        }
+    }
+
+    /**
+     * 增加标签接口
+     * @param houseId
+     * @param tag
+     * @return
+     */
+    @PostMapping("admin/house/tag")
+    @ResponseBody
+    public ResponseEntity addHouseTag(@RequestParam(value = "house_id") Long houseId,
+                                   @RequestParam(value = "tag") String tag) {
+        if (houseId < 1 || Strings.isNullOrEmpty(tag)) {
+            return ResponseEntity.createByErrorCodeMessage(StatusCode.BAD_REQUEST);
+        }
+
+        ServiceResult result = this.houseService.addTag(houseId, tag);
+        if (result.isSuccess()) {
+            return ResponseEntity.createBySuccess();
+        } else {
+            return ResponseEntity.createByErrorCodeMessage(HttpStatus.BAD_REQUEST.value(), result.getMessage());
+        }
+    }
+
+    /**
+     * 移除标签接口
+     * @param houseId
+     * @param tag
+     * @return
+     */
+    @DeleteMapping("admin/house/tag")
+    @ResponseBody
+    public ResponseEntity removeHouseTag(@RequestParam(value = "house_id") Long houseId,
+                                      @RequestParam(value = "tag") String tag) {
+        if (houseId < 1 || Strings.isNullOrEmpty(tag)) {
+            return ResponseEntity.createByErrorCodeMessage(StatusCode.BAD_REQUEST);
+        }
+
+        ServiceResult result = this.houseService.removeTag(houseId, tag);
+        if (result.isSuccess()) {
+            return ResponseEntity.createBySuccess();
+        } else {
+            return ResponseEntity.createByErrorCodeMessage(HttpStatus.BAD_REQUEST.value(), result.getMessage());
+        }
+    }
+
+    /**
+     * 审核接口
+     * @param id
+     * @param operation
+     * @return
+     */
+    @PutMapping("admin/house/operate/{id}/{operation}")
+    @ResponseBody
+    public ResponseEntity operateHouse(@PathVariable(value = "id") Long id,
+                                    @PathVariable(value = "operation") int operation) {
+        if (id <= 0) {
+            return ResponseEntity.createByErrorCodeMessage(StatusCode.NOT_VALID_PARAM);
+        }
+        ServiceResult result;
+
+        switch (operation) {
+            case HouseOperation.PASS:
+                result = this.houseService.updateStatus(id, HouseStatus.PASSES.getValue());
+                break;
+            case HouseOperation.PULL_OUT:
+                result = this.houseService.updateStatus(id, HouseStatus.NOT_AUDITED.getValue());
+                break;
+            case HouseOperation.DELETE:
+                result = this.houseService.updateStatus(id, HouseStatus.DELETED.getValue());
+                break;
+            case HouseOperation.RENT:
+                result = this.houseService.updateStatus(id, HouseStatus.RENTED.getValue());
+                break;
+            default:
+                return ResponseEntity.createByErrorCodeMessage(StatusCode.BAD_REQUEST);
+        }
+
+        if (result.isSuccess()) {
+            return ResponseEntity.ofSuccess(null);
+        }
+        return ResponseEntity.createByErrorCodeMessage(HttpStatus.BAD_REQUEST.value(),
+                result.getMessage());
     }
 
 }
