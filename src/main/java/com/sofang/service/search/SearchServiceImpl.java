f52c9f49787e6626f65f07c4af23cabbe896e8d9
qianMu7 <849447894@qq.com>
2018-06-12 23:57:41 +0800

    Code refactoring, add ES.

--- src/main/java/com/sofang/service/search/SearchServiceImpl.java
+++ src/main/java/com/sofang/service/search/SearchServiceImpl.java
@@ -1,18 +1,38 @@
 package com.sofang.service.search;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
 import com.sofang.entity.House;
 import com.sofang.repository.HouseRepository;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.rest.RestStatus;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class SearchServiceImpl implements SearchService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SearchServiceImpl.class);
+
+    @Autowired
+    private TransportClient esClient;
+
+    private static final String INDEX_NAME = "fang";
+
+    private static final String INDEX_TYPE = "house";
 
     @Autowired
     private HouseRepository houseRepository;
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public void index(Long houseId) {
@@ -22,6 +42,19 @@ public class SearchServiceImpl implements SearchService {
         modelMapper.map(house, houseIndexTemplate);
 
     }
+
+    private boolean create(HouseIndexTemplate indexTemplate){
+        try {
+            IndexResponse response = this.esClient.prepareIndex(INDEX_NAME, INDEX_TYPE)
+                    .setSource(objectMapper.writeValueAsBytes(indexTemplate), XContentType.JSON).get();
+            logger.debug("Create index with house " + indexTemplate.getHouseId());
+            return response.status() == RestStatus.CREATED;
+        } catch (JsonProcessingException e) {
+            logger.error("Error to Index house " + indexTemplate.getHouseId(), e);
+            return false;
+        }
+    }
+
 
     @Override
     public void remove(Long houseId) {
