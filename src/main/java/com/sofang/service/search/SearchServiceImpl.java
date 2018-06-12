df43754736ffc5712d9dc9e1e3c29a18c6a79251
qianMu7 <849447894@qq.com>
2018-06-13 00:21:07 +0800

    Code refactoring, add ES.

--- src/main/java/com/sofang/service/search/SearchServiceImpl.java
+++ src/main/java/com/sofang/service/search/SearchServiceImpl.java
@@ -3,16 +3,29 @@ package com.sofang.service.search;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import com.sofang.entity.House;
+import com.sofang.entity.HouseTag;
 import com.sofang.repository.HouseRepository;
+import com.sofang.repository.HouseTagRepository;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+import org.elasticsearch.index.reindex.DeleteByQueryRequestBuilder;
 import org.elasticsearch.rest.RestStatus;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 public class SearchServiceImpl implements SearchService {
 
@@ -29,6 +42,9 @@ public class SearchServiceImpl implements SearchService {
     private HouseRepository houseRepository;
 
     @Autowired
+    private HouseTagRepository tagRepository;
+
+    @Autowired
     private ModelMapper modelMapper;
 
     @Autowired
@@ -40,6 +56,13 @@ public class SearchServiceImpl implements SearchService {
         Preconditions.checkNotNull(house, "Index house " + houseId + " dose not exsit!");
         HouseIndexTemplate houseIndexTemplate = new HouseIndexTemplate();
         modelMapper.map(house, houseIndexTemplate);
+
+        List<HouseTag> tags = tagRepository.findAllByHouseId(houseId);
+        if(!CollectionUtils.isEmpty(tags)){
+            List<String> tagStrings = Lists.newArrayList();
+            tags.forEach(tag -> tagStrings.add(tag.getName()));
+            houseIndexTemplate.setTags(tagStrings);
+        }
 
     }
 
@@ -55,9 +78,35 @@ public class SearchServiceImpl implements SearchService {
         }
     }
 
+    private boolean update(String esId, HouseIndexTemplate indexTemplate) {
+        try {
+            UpdateResponse response = this.esClient.prepareUpdate(INDEX_NAME, INDEX_TYPE, esId)
+                    .setDoc(objectMapper.writeValueAsBytes(indexTemplate), XContentType.JSON).get();
+            logger.debug("Update index with house: " + indexTemplate.getHouseId());
+            return response.status() == RestStatus.OK;
+        } catch (JsonProcessingException e) {
+            logger.error("Error to index house " + indexTemplate.getHouseId(), e);
+            return false;
+        }
+    }
+
+    private boolean deleteAndCreate(long totalHit, HouseIndexTemplate indexTemplate){
+        DeleteByQueryRequestBuilder builder = DeleteByQueryAction.INSTANCE.newRequestBuilder(esClient)
+                .filter(QueryBuilders.termQuery(HouseIndexKey.HOUSE_ID, indexTemplate.getHouseId())).source(INDEX_NAME);
+        logger.debug("Delete by query for house : " + builder);
+
+        BulkByScrollResponse response = builder.get();
+        long deleted = response.getDeleted();
+        if (deleted != totalHit) {
+            logger.warn("Need delete {}, but {} was deleted!", totalHit, deleted);
+            return false;
+        } else {
+            return create(indexTemplate);
+        }
+    }
+
 
     @Override
     public void remove(Long houseId) {
-
     }
 }
